65e94092360ab4ab7299ccc8416961945410c700
Abhijeet Soni <abhisonijeet123@gmail.com>
2025-09-30 15:19:24 +0530

    new nodeAgentService

--- nodeAgentService.ts
+++ nodeAgentService.ts
@@ -97,7 +97,6 @@ export async function nodeAgent(
  
   try {
  
-    // Naya: Yahan React Agent ko check karein
     if (node.agentName === "reactAgent") {
       console.log('[Decision] Redirecting to ReAct Agent from Node-RED.');
       const result =await executeReactAgentNode(
